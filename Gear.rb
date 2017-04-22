06e85de2a5967f7281fcb8462f2af541752dfa7e
Daisy-Clown <another.ruler@gmail.com>
2017-04-22 15:14:00 +0900

    Wheel独立

--- Gear.rb
+++ Gear.rb
@@ -1,25 +1,37 @@
 class Gear
     attr_reader :chainring, :cog, :wheel
-    def initialize(chainring, cog, rim, tire)
+    def initialize(chainring, cog, wheel=nil)
 	@chainring = chainring
 	@cog = cog
-    @wheel = wheel.new(rim, tire)
+    @wheel = wheel
    end
+    
  def ratio
  	chainring / cog.to_f #float型へのキャスト
    end
     def gear_inches
      ratio * wheel.diameter
    end
-    wheel = Struct.new(:rim, :tire)do
-    def diameter
-      rim * (tire * 2)
     end
-   end
 
-puts Gear.new(52, 11,26,1.5).gear_inches
-puts Gear.new(52, 11,24,1.25).gear_inches
+class Wheel
+    attr_reader :rim, :tire
+    def initialize(rim, tire)
+        @rim = rim
+        @tire = tire
+    end
+    def diameter
+        rim + (tire * 2)
+        end
+    def circumference
+        diameter * Math::PI
+    end
+end
+wheel = Wheel.new(34, 20)
+puts Gear.new(23, 10, wheel).gear_inches
+
+#puts Gear.new(52, 11,26,1.5).gear_inches
+#puts Gear.new(52, 11,24,1.25).gear_inches
 
 #puts Gear.new(52, 11).ratio
 #puts Gear.new(38, 22).ratio
- 
