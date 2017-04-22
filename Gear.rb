9f661a918c70cc98ef9f326c73a258e8240921b6
Daisy-Clown <another.ruler@gmail.com>
2017-04-22 14:56:52 +0900

    単一責任について

--- Gear.rb
+++ Gear.rb
@@ -1,18 +1,21 @@
 class Gear
-    attr_reader :chainring, :cog, :rim, :tire
+    attr_reader :chainring, :cog, :wheel
     def initialize(chainring, cog, rim, tire)
 	@chainring = chainring
 	@cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel.new(rim, tire)
    end
  def ratio
  	chainring / cog.to_f #float型へのキャスト
    end
     def gear_inches
-     ratio * (rim + (tire * 2))
+     ratio * wheel.diameter
    end
- end
+    wheel = Struct.new(:rim, :tire)do
+    def diameter
+      rim * (tire * 2)
+    end
+   end
 
 puts Gear.new(52, 11,26,1.5).gear_inches
 puts Gear.new(52, 11,24,1.25).gear_inches
