66ef0f30a59f18a76237aeb0bcdeda63436e8ff8
Daisy-Clown <another.ruler@gmail.com>
2017-04-22 14:31:21 +0900

    Gear edit

--- Gear.rb
+++ Gear.rb
@@ -1,6 +1,6 @@
 class Gear
     attr_reader :chainring, :cog, :rim, :tire
-	def initialize(chainring, cog)
+    def initialize(chainring, cog, rim, tire)
 	@chainring = chainring
 	@cog = cog
     @rim = rim
@@ -15,7 +15,7 @@ class Gear
  end
 
 puts Gear.new(52, 11,26,1.5).gear_inches
-puts Gear.new(52, 11,1.25).gear_inches
+puts Gear.new(52, 11,24,1.25).gear_inches
 
 #puts Gear.new(52, 11).ratio
 #puts Gear.new(38, 22).ratio
